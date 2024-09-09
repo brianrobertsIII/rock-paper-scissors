4298a5e58ad3c105f89237bf2459af9f29857a31
brianrobertsIII <171775024+brianrobertsIII@users.noreply.github.com>
2024-09-09 13:13:16 -0500

    added javascript alerts

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-function getComputerChoice() {
-    let choice = Math.random();
-
-    if(choice < 0.25){
-        return 1;
-    }
-
-    else if(choice < 0.50){
-        return 2;
-    }
-
-    else if(choice < 0.75){
-        return 3;
-    }
-}
+alert('ROCK!')
+alert('PAPER!')
+alert('SCISSORS!')
+alert('GO!!')
