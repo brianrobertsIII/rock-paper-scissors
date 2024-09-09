a5f8c93a99ef5a0f1ac1daa38f817ad790543cc7
brianrobertsIII <171775024+brianrobertsIII@users.noreply.github.com>
2024-09-09 15:25:32 -0500

    updated js

--- game.js
+++ game.js
@@ -11,9 +11,46 @@ function getComputerChoice() {
     return choice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    
+function checkWinner(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection) {
+        return 'Tie!';
+    }
+
+    else if(
+        (playerSelection == 'Rock' && computerSelection == 'Scissors') || 
+        (playerSelection == 'Scissors' && computerSelection == 'Paper') ||
+        (playerSelection == 'Paper' && computerSelection == 'Rock')
+    ) {
+        return 'Player Win!'
+    }    
+
+    else {
+        return 'Computer Win!'
+    }
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == 'Tie') {
+        return 'It is a Tie!'
+    }
 
+    else if(result == 'Player') {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+
+    else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+function playGame() {
+    console.log('Welcome!')
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = 'Rock';
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }    
+}
+
+playGame()
